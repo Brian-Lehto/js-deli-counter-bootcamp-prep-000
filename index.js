64a9e8cfd178524a74d613a2c9caa10a8e256158
Brian  Lehto <bdlden1978@outlook.com>
2018-01-12 03:27:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var katzDeli = []
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+function nowServing(katzDeliLine, name) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    var name = katzDeliLine.shift(0)
+    return `Currently serving ${name}.`
+  }
+}
+function currentLine(katzDeliLine) {
+  if (katzDeliLine === 0) {
+    return 'The line is currently empty.'
+  } else {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      return `The line is currently ${katzDeliLine[i] + 1}`
+    }
+  }
+}
